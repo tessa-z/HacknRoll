6889770bed2092dcb3d1fb03114073dded2a1aa9
tessa-z <yuehan9902@gmail.com>
2021-01-08 20:21:07 +0800

    Code cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -201,9 +201,6 @@ class CurvedListItem extends StatelessWidget {
   }
 }
 
-
-
-
 //////////////////////////////////////////////////////////////////////////////
 class Music extends StatefulWidget {
   @override
@@ -228,58 +225,50 @@ class _MusicState extends State<Music> {
             style: TextStyle(
                 fontSize: 30, color: Colors.blue, fontWeight: FontWeight.bold),
           ),
-          // ListTile(
-          //   title: Text(songList[0]),
-          //   leading: Radio(
-          //     value: 0,
-          //     groupValue: _song,
-          //     onChanged: (value) {
-          //       this.setState((){
-          //         _song = value;
-          //       });
-          //     },
-          //   ),
-          // ),
-          // ListTile(
-          //   title: Text(songList[1]),
-          //   leading: Radio(
-          //     value: 1,
-          //     groupValue: _song,
-          //     onChanged: (value) {
-          //       this.setState((){
-          //         _song = value;
-          //       });
-          //     },
-          //   ),
-          // ),
           ListView.builder(
             scrollDirection: Axis.vertical,
             shrinkWrap: true,
             itemCount: songList.length,
             itemBuilder: (BuildContext context, int index) {
-              return new ListTile(
-                title: Text(songList[index]),
-                leading: Radio(
-                  value: index,
-                  groupValue: _song,
-                  onChanged: (value) {
-                    this.setState(() {
-                      _song = value;
-                    });
-                  },
-                ),
+              return new
+              // GestureDetector(
+              //   onTap: () {
+              //     setState(() {
+              //       debugPrint(context.widget.toString());
+              //     });
+              //   },
+              //   child:
+                ListTile(
+                  title: Text(songList[index]),
+                  leading: Radio(
+                    value: index,
+                    groupValue: _song,
+                    onChanged: (value) {
+                      setState(() {
+                        _song = value;
+                      });
+                    },
+                  ),
               );
+              // );
             },
-          )
+          ),
         ],
       ),
     );
 
-
   }
 }
 
-class Water extends StatelessWidget {
+class Water extends StatefulWidget {
+  @override
+  _WaterState createState() => _WaterState();
+}
+
+class _WaterState extends State<Water> {
+
+  var _waterVolume = 0;
+
   @override
   Widget build(BuildContext context) {
     return Container(
