392445e83e5f6780cefc0da3c3f9498d6371b7db
tessa-z <yuehan9902@gmail.com>
2021-01-09 00:32:50 +0800

    Add bluetooth screen for development purposes

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
+import 'helperFunctions.dart' as helper;
 
 void main() => runApp(MyApp());
 
@@ -34,7 +35,7 @@ class MyStatefulWidget extends StatefulWidget {
 /// This is the private State class that goes with MyStatefulWidget.
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   int _selectedIndex = 0;
-  List<Widget> screens = [Clock(), Music(), Water(), About()];
+  List<Widget> screens = [Clock(), Music(), Water(), About(), Bluetooth()];
 
   @override
   Widget build(BuildContext context) {
@@ -74,22 +75,21 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
                 selectedIcon: Icon(Icons.book),
                 label: Text('Tasks'),
               ),
+              NavigationRailDestination(
+                icon: Icon(Icons.bluetooth),
+                selectedIcon: Icon(Icons.bluetooth),
+                label: Text('Bluetooth Test'),
+              ),
             ],
           ),
           VerticalDivider(thickness: 1, width: 1),
           // This is the main content.
-          Expanded(
-              child: screens[_selectedIndex]
-          ),
+          Expanded(child: screens[_selectedIndex]),
         ],
       ),
     );
   }
 }
-
-
-
-
 
 //////////////////////////////////////////////////////////////////////////////
 
@@ -176,9 +176,7 @@ class CurvedListItem extends StatelessWidget {
           borderRadius: const BorderRadius.only(
             bottomLeft: Radius.circular(0.0),
           ),
-          border: Border.all(
-              width: 3.0
-          ),
+          border: Border.all(width: 3.0),
         ),
         padding: const EdgeInsets.only(
           left: 60,
@@ -212,17 +210,13 @@ class _MusicState extends State<Music> {
   var _song = 0;
   var songList = ["Pixel Galaxy", "Sunday", "Snailchan Adventure"];
 
-  FlutterBlue flutterBlue = FlutterBlue.instance;
-
   @override
   Widget build(BuildContext context) {
-
     return Container(
       padding: EdgeInsets.all(12.0),
       child: ListView(
         children: <Widget>[
-          Row(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[
-          ]),
+          Row(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[]),
           Text(
             "Choose Your Music!",
             style: TextStyle(
@@ -233,33 +227,23 @@ class _MusicState extends State<Music> {
             shrinkWrap: true,
             itemCount: songList.length,
             itemBuilder: (BuildContext context, int index) {
-              return new
-              // GestureDetector(
-              //   onTap: () {
-              //     setState(() {
-              //       debugPrint(context.widget.toString());
-              //     });
-              //   },
-              //   child:
-                ListTile(
-                  title: Text(songList[index]),
-                  leading: Radio(
-                    value: index,
-                    groupValue: _song,
-                    onChanged: (value) {
-                      setState(() {
-                        _song = value;
-                      });
-                    },
-                  ),
+              return new ListTile(
+                title: Text(songList[index]),
+                leading: Radio(
+                  value: index,
+                  groupValue: _song,
+                  onChanged: (value) {
+                    setState(() {
+                      _song = value;
+                    });
+                  },
+                ),
               );
-              // );
             },
           ),
         ],
       ),
     );
-
   }
 }
 
@@ -269,8 +253,8 @@ class Water extends StatefulWidget {
 }
 
 class _WaterState extends State<Water> {
-
   var _waterVolume = 0;
+  final _counterFormKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -279,19 +263,52 @@ class _WaterState extends State<Water> {
       alignment: Alignment.topLeft,
       child: ListView(
         children: <Widget>[
-          Row(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[
-          ]),
+          Row(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[]),
           Text(
             "Drink Up!",
             style: TextStyle(
                 fontSize: 30, color: Colors.blue, fontWeight: FontWeight.bold),
           ),
+          Form(
+              key: _counterFormKey,
+              child: Column(children: <Widget>[
+                // Add TextFormFields and ElevatedButton here.
+                Text('How much water will wake you up tomorrow?'),
+                TextFormField(
+                  // The validator receives the text that the user has entered.
+                  decoration: const InputDecoration(
+                    hintText: 'Recommended 650ml',
+                    labelText: 'Volume (ml)'
+                  ),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Drink up or else';
+                    }
+                    if (!helper.isNumeric(value)) {
+                      return ('You want to drink $value?');
+                    }
+                    return null;
+                  },
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // Validate returns true if the form is valid, otherwise false.
+                    if (_counterFormKey.currentState.validate()) {
+                      // If the form is valid, display a snackbar. In the real world,
+                      // you'd often call a server or save the information in a database.
+
+                      Scaffold.of(context).showSnackBar(
+                          SnackBar(content: Text('Sending data')));
+                    }
+                  },
+                  child: Text('Confirm'),
+                )
+              ]))
         ],
       ),
     );
   }
 }
-
 
 class About extends StatelessWidget {
   @override
@@ -301,8 +318,7 @@ class About extends StatelessWidget {
       alignment: Alignment.topLeft,
       child: ListView(
         children: <Widget>[
-          Row(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[
-          ]),
+          Row(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[]),
           Text(
             "Plan Your Day!",
             style: TextStyle(
@@ -314,5 +330,70 @@ class About extends StatelessWidget {
   }
 }
 
+class Bluetooth extends StatefulWidget {
+  @override
+  _BluetoothState createState() => _BluetoothState();
+}
 
+class _BluetoothState extends State<Bluetooth> {
+  final FlutterBlue flutterBlue = FlutterBlue.instance;
+  final List<BluetoothDevice> devicesList = new List<BluetoothDevice>();
+  final Map<Guid, List<int>> readValues = new Map<Guid, List<int>>();
+  final textController = TextEditingController();
+  BluetoothDevice connectedDevice;
+  List<BluetoothService> bluetoothServices;
 
+  _showDeviceTolist(final BluetoothDevice device) {
+    if (!devicesList.contains(device)) {
+      setState(() {
+        devicesList.add(device);
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    // Start scanning
+    flutterBlue.startScan(timeout: Duration(seconds: 4));
+
+    // Listen to scan results
+    flutterBlue.scanResults.listen((results) {
+      // do something with scan results
+      for (ScanResult r in results) {
+        _showDeviceTolist(r.device);
+      }
+    });
+
+    // Stop scanning
+    flutterBlue.stopScan();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      padding: EdgeInsets.all(12.0),
+      alignment: Alignment.topLeft,
+      child: ListView(
+        children: <Widget>[
+          Row(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[]),
+          Text(
+            "Bluetooth Connection",
+            style: TextStyle(
+                fontSize: 30, color: Colors.blue, fontWeight: FontWeight.bold),
+          ),
+          ListView.builder(
+            scrollDirection: Axis.vertical,
+            shrinkWrap: true,
+            itemCount: devicesList.length,
+            itemBuilder: (BuildContext context, int index) {
+              return new ListTile(
+                title: Text(devicesList[index].toString())
+                );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
