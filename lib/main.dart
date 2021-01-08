8c2a0f33d1528b852e2fe31c51c6120edb1ad386
prav17 <77115497+prav17@users.noreply.github.com>
2021-01-08 13:08:42 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,242 @@
+/// Flutter code sample for NavigationRail
+
+// This example shows a [NavigationRail] used within a Scaffold with 3
+// [NavigationRailDestination]s. The main content is separated by a divider
+// (although elevation on the navigation rail can be used instead). The
+// `_selectedIndex` is updated by the `onDestinationSelected` callback.
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+/// This is the main application widget.
+class MyApp extends StatelessWidget {
+  static const String _title = 'Flutter Code Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: MyStatefulWidget(),
+    );
+  }
+}
+
+/// This is the stateful widget that the main application instantiates.
+class MyStatefulWidget extends StatefulWidget {
+  MyStatefulWidget({Key key}) : super(key: key);
+
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+/// This is the private State class that goes with MyStatefulWidget.
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 0;
+  List<Widget> screens = [Featured(), Newest(), Collection(), About()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Morning Pal'),
+      ),
+      body: Row(
+        children: <Widget>[
+          NavigationRail(
+            selectedIndex: _selectedIndex,
+            onDestinationSelected: (int index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
+            labelType: NavigationRailLabelType.selected,
+            destinations: [
+              NavigationRailDestination(
+                icon: Icon(Icons.access_alarm),
+                selectedIcon: Icon(Icons.access_alarm),
+                label: Text('Alarm'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.library_music),
+                selectedIcon: Icon(Icons.library_music),
+                label: Text('Music'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.star_border),
+                selectedIcon: Icon(Icons.star),
+                label: Text('Water'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.book),
+                selectedIcon: Icon(Icons.book),
+                label: Text('Tasks'),
+              ),
+            ],
+          ),
+          VerticalDivider(thickness: 1, width: 1),
+          // This is the main content.
+          Expanded(
+              child: screens[_selectedIndex]
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////
+
+class Featured extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Choose Your Alarm!'),
+      ),
+      body: ListView(
+        scrollDirection: Axis.vertical,
+        children: <Widget>[
+          CurvedListItem(
+            title: '05:00',
+            color: Colors.red,
+            nextColor: Colors.green,
+          ),
+          CurvedListItem(
+            title: '05:30',
+            color: Colors.green,
+            nextColor: Colors.yellow,
+          ),
+          CurvedListItem(
+            title: '06:00',
+            color: Colors.yellow,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CurvedListItem extends StatelessWidget {
+  const CurvedListItem({
+    this.title,
+    this.icon,
+    this.people,
+    this.color,
+    this.nextColor,
+  });
+
+  final String title;
+  final String people;
+  final IconData icon;
+  final Color color;
+  final Color nextColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: nextColor,
+      child: Container(
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: const BorderRadius.only(
+            bottomLeft: Radius.circular(80.0),
+          ),
+        ),
+        padding: const EdgeInsets.only(
+          left: 32,
+          top: 80.0,
+          bottom: 50,
+        ),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                title,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold),
+              ),
+              Row(),
+            ]),
+      ),
+    );
+  }
+}
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////
+class Newest extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(12.0),
+      child: ListView(
+        children: <Widget>[
+          Row(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[
+          ]),
+          Text(
+            "Choose Your Music!",
+            style: TextStyle(
+                fontSize: 30, color: Colors.blue, fontWeight: FontWeight.bold),
+
+          ),
+
+        ],
+      ),
+    );
+  }
+}
+
+class Collection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(12.0),
+      alignment: Alignment.topLeft,
+      child: ListView(
+        children: <Widget>[
+          Row(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[
+          ]),
+          Text(
+            "Drink Up!",
+            style: TextStyle(
+                fontSize: 30, color: Colors.blue, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+class About extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(12.0),
+      alignment: Alignment.topLeft,
+      child: ListView(
+        children: <Widget>[
+          Row(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[
+          ]),
+          Text(
+            "Plan Your Day!",
+            style: TextStyle(
+                fontSize: 30, color: Colors.blue, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
