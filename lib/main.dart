74ace4c1b317194f75a6127bbe33742d98b5b774
tessa-z <yuehan9902@gmail.com>
2021-01-09 08:51:33 +0800

    Find and connect successfully to bt module

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
+import 'dart:async';
+import 'dart:typed_data';
+import 'dart:convert';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+import 'package:flutter/material.dart';
+import 'helperFunctions.dart' as helper;
+
 /// Flutter code sample for NavigationRail
 // This example shows a [NavigationRail] used within a Scaffold with 3
 // [NavigationRailDestination]s. The main content is separated by a divider
 // (although elevation on the navigation rail can be used instead). The
 // `_selectedIndex` is updated by the `onDestinationSelected` callback.
-
-import 'package:flutter/material.dart';
-import 'package:flutter_blue/flutter_blue.dart';
-import 'helperFunctions.dart' as helper;
 
 void main() => runApp(MyApp());
 
@@ -271,69 +274,233 @@ class About extends StatelessWidget {
   }
 }
 
+class BluetoothDeviceListEntry extends ListTile {
+  BluetoothDeviceListEntry({
+    @required BluetoothDevice device,
+    int rssi,
+    GestureTapCallback onTap,
+    GestureLongPressCallback onLongPress,
+    bool enabled = true,
+  }) : super(
+    onTap: onTap,
+    onLongPress: onLongPress,
+    enabled: enabled,
+    leading:
+    Icon(Icons.devices),
+    title: Text(device.name ?? "Unknown device"),
+    subtitle: Text(device.address.toString()),
+    trailing: Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        rssi != null
+            ? Container(
+          margin: new EdgeInsets.all(8.0),
+          child: DefaultTextStyle(
+            style: _computeTextStyle(rssi),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text(rssi.toString()),
+                Text('dBm'),
+              ],
+            ),
+          ),
+        )
+            : Container(width: 0, height: 0),
+        device.isConnected
+            ? Icon(Icons.import_export)
+            : Container(width: 0, height: 0),
+        device.isBonded
+            ? Icon(Icons.link)
+            : Container(width: 0, height: 0),
+      ],
+    ),
+  );
+
+  static TextStyle _computeTextStyle(int rssi) {
+    /**/ if (rssi >= -35)
+      return TextStyle(color: Colors.greenAccent[700]);
+    else if (rssi >= -45)
+      return TextStyle(
+          color: Color.lerp(
+              Colors.greenAccent[700], Colors.lightGreen, -(rssi + 35) / 10));
+    else if (rssi >= -55)
+      return TextStyle(
+          color: Color.lerp(
+              Colors.lightGreen, Colors.lime[600], -(rssi + 45) / 10));
+    else if (rssi >= -65)
+      return TextStyle(
+          color: Color.lerp(Colors.lime[600], Colors.amber, -(rssi + 55) / 10));
+    else if (rssi >= -75)
+      return TextStyle(
+          color: Color.lerp(
+              Colors.amber, Colors.deepOrangeAccent, -(rssi + 65) / 10));
+    else if (rssi >= -85)
+      return TextStyle(
+          color: Color.lerp(
+              Colors.deepOrangeAccent, Colors.redAccent, -(rssi + 75) / 10));
+    else
+      /*code symetry*/
+      return TextStyle(color: Colors.redAccent);
+  }
+}
+
 class Bluetooth extends StatefulWidget {
+  final bool start = false;
   @override
   _BluetoothState createState() => _BluetoothState();
 }
 
 class _BluetoothState extends State<Bluetooth> {
-  final FlutterBlue flutterBlue = FlutterBlue.instance;
-  final List<BluetoothDevice> devicesList = new List<BluetoothDevice>();
-  final Map<Guid, List<int>> readValues = new Map<Guid, List<int>>();
-  final textController = TextEditingController();
-  BluetoothDevice connectedDevice;
-  List<BluetoothService> bluetoothServices;
+  StreamSubscription<BluetoothDiscoveryResult> _streamSubscription;
+  List<BluetoothDiscoveryResult> results = List<BluetoothDiscoveryResult>();
+  bool isDiscovering;
 
-  _showDeviceTolist(final BluetoothDevice device) {
-    if (!devicesList.contains(device)) {
-      setState(() {
-        devicesList.add(device);
-      });
-    }
-  }
+  _BluetoothState();
 
   @override
   void initState() {
-    // Start scanning
-    flutterBlue.startScan(timeout: Duration(seconds: 4));
+    super.initState();
 
-    // Listen to scan results
-    flutterBlue.scanResults.listen((results) {
-      // do something with scan results
-      for (ScanResult r in results) {
-        _showDeviceTolist(r.device);
-      }
+    isDiscovering = widget.start;
+    if (isDiscovering) {
+      _startDiscovery();
+    }
+  }
+
+  void _restartDiscovery() {
+    setState(() {
+      results.clear();
+      isDiscovering = true;
     });
 
-    // Stop scanning
-    flutterBlue.stopScan();
+    _startDiscovery();
+  }
+
+  void _startDiscovery() {
+    _streamSubscription =
+        FlutterBluetoothSerial.instance.startDiscovery().listen((r) {
+          setState(() {
+            results.add(r);
+          });
+        });
+
+    _streamSubscription.onDone(() {
+      setState(() {
+        isDiscovering = false;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    // Avoid memory leak (`setState` after dispose) and cancel discovery
+    _streamSubscription?.cancel();
+
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-
-    return Container(
-      padding: EdgeInsets.all(12.0),
-      alignment: Alignment.topLeft,
-      child: ListView(
-        children: <Widget>[
-          Row(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[]),
-          Text(
-            "Bluetooth Connection",
-            style: TextStyle(
-                fontSize: 30, color: Colors.blue, fontWeight: FontWeight.bold),
-          ),
-          ListView.builder(
-            scrollDirection: Axis.vertical,
-            shrinkWrap: true,
-            itemCount: devicesList.length,
-            itemBuilder: (BuildContext context, int index) {
-              return new ListTile(
-                title: Text(devicesList[index].toString())
-                );
-            },
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: isDiscovering
+            ? Text('Discovering')
+            : Text('Discovered'),
+        actions: <Widget>[
+          isDiscovering
+              ? FittedBox(
+            child: Container(
+              margin: new EdgeInsets.all(16.0),
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+              ),
+            ),
+          )
+              : IconButton(
+            icon: Icon(Icons.replay),
+            onPressed: _restartDiscovery,
+          )
         ],
+      ),
+      body: ListView.builder(
+        itemCount: results.length,
+        itemBuilder: (BuildContext context, index) {
+          BluetoothDiscoveryResult result = results[index];
+          return BluetoothDeviceListEntry(
+            device: result.device,
+            rssi: result.rssi,
+            onTap: () async {
+              // Some simplest connection :F
+              try {
+                BluetoothConnection connection = await BluetoothConnection.toAddress(result.device.address);
+                print('Connected to the device');
+
+                connection.input.listen((Uint8List data) {
+                  print('Data incoming: ${ascii.decode(data)}');
+                  connection.output.add(data); // Sending data
+
+                  if (ascii.decode(data).contains('!')) {
+                    connection.finish(); // Closing connection
+                    print('Disconnecting by local host');
+                  }
+                }).onDone(() {
+                  print('Disconnected by remote request');
+                });
+              }
+              catch (exception) {
+                print('Cannot connect, exception occurred');
+              }
+            },
+            onLongPress: () async {
+              try {
+                bool bonded = false;
+                if (result.device.isBonded) {
+                  print('Unbonding from ${result.device.address}...');
+                  await FlutterBluetoothSerial.instance
+                      .removeDeviceBondWithAddress(result.device.address);
+                  print('Unbonding from ${result.device.address} has succed');
+                } else {
+                  print('Bonding with ${result.device.address}...');
+                  bonded = await FlutterBluetoothSerial.instance
+                      .bondDeviceAtAddress(result.device.address);
+                  print(
+                      'Bonding with ${result.device.address} has ${bonded ? 'succed' : 'failed'}.');
+                }
+                setState(() {
+                  results[results.indexOf(result)] = BluetoothDiscoveryResult(
+                      device: BluetoothDevice(
+                        name: result.device.name ?? '',
+                        address: result.device.address,
+                        type: result.device.type,
+                        bondState: bonded
+                            ? BluetoothBondState.bonded
+                            : BluetoothBondState.none,
+                      ),
+                      rssi: result.rssi);
+                });
+              } catch (ex) {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: const Text('Error occurred while bonding'),
+                      content: Text("${ex.toString()}"),
+                      actions: <Widget>[
+                        new FlatButton(
+                          child: new Text("Close"),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                      ],
+                    );
+                  },
+                );
+              }
+            },
+          );
+        },
       ),
     );
   }
